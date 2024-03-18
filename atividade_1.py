cec6260da7d7e1905da292dbec1f09bd66fc56f1
caioschaedler <163935333+caioschaedler@users.noreply.github.com>
2024-03-18 19:55:23 -0300

    Add files via upload

---
+++ atividade_1.py
@@ -0,0 +1,12 @@
+###
+# atividade 1 
+# Aluno: caio schaedler
+# data 11/03/2024
+###
+ 
+num1 = float(input ('digite o 1o numero: '))
+num2 = float(input ('digite o 2o numero: '))
+num3 = float(input ('digite o 3o numero: '))
+num4 = float(input ('digite o 4o numero: '))
+media = (num1+num2+num3+num4) / 4 
+print(f' a media eh: {media}')
---
+++ atividade_9.py
@@ -0,0 +1,3 @@
+nota1 = float (input('digite a nota da prova 1:'))
+nota2 = ((7 * 3 ) - nota1) / 2
+print(f' voce deve tirar mais que {nota2} para ser aprovado')
